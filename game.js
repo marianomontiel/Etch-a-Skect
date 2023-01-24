53cd1f53af9fca2b55db43d368760e0dcee1db21
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-24 16:52:47 -0300

    addEventListener to divs

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container');
 const buttons = document.querySelector('.buttons');
+const columns = 16;
+const rows = 16;
 
 container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 40px); grid-template-rows: repeat(16, 40px);');
 
@@ -8,5 +10,9 @@ for (let i = 0; i < (16 * 16) ; i++) {
     div.setAttribute('style', 'border: 1px solid white;');
     container.appendChild(div);
     container.insertBefore(div, buttons);
+    div.addEventListener('click', () => {
+        div.setAttribute('style', 'background-color: black;');
+      });
 }
 
+addEventListener
