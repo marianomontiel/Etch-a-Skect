91a31b5ce0e8f03053a72f99e26e24b020f5a7fb
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-30 07:31:47 -0300

    Make color picker functional

--- game.js
+++ game.js
@@ -3,12 +3,12 @@ const size = 32;
 var screenSize = '360';
 
 //Add button to prompt for desired resolution
-const resolution = document.querySelector('#resolution');
+var resolution = document.querySelector('#resolution');
 resolution.addEventListener('click', () => { resolutionChange(); });
 
 //Function for resolution prompt and DOM manipulation function
 function resolutionChange() {
-  const resolution = prompt('Choose screen resoltuion!');
+  resolution = prompt('Choose screen resoltuion!');
   if (resolution > 0 && resolution <= 100) {
     container.setAttribute('style', `display: grid; grid-template-columns: repeat(${resolution}, ${(screenSize / resolution)}px); grid-template-rows: repeat(${resolution}, ${(screenSize / resolution)}px);`);
 
@@ -35,7 +35,7 @@ function createDivs(a) {
     var isMousedown = false;
     div.addEventListener("mousedown", () => {
       isMousedown = true;
-      div.setAttribute('style', 'background-color: black;');
+      div.setAttribute('style', `background-color: ${value};`);
     });
     div.addEventListener("mouseup", () => {
       isMousedown = false;
@@ -43,7 +43,7 @@ function createDivs(a) {
     div.addEventListener("mousemove", () => {
       if (isMousedown) {
         if (body.style.background === 'rgb(32, 32, 32)') {
-          div.setAttribute('style', 'background-color: black;');
+          div.setAttribute('style', `background-color: ${value};`);
         } else {
           div.setAttribute('style', `background-color: rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}); border:1px solid lightgrey`);
         }
@@ -75,3 +75,7 @@ magic.addEventListener('click', () => {
     body.setAttribute('style', 'background: rgb(32, 32, 32)');
   }
 });
+
+//Color picker function
+var value = document.querySelector('#picker');
+value.addEventListener('input', () => {value = document.querySelector('#picker').value; createDivs(resolution)});
